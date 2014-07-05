6014a209a82172f91a2f95d1487a735cc59e7515
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-07-05 22:50:12 +0800

    Breaking up the routing to more files.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-var ctrl = require('../app_server/controllers/main');
-
-module.exports = function (app) {
-	app.get('/', ctrl.index);
+module.exports = function(app) {
+	require('./main')(app);
 };
