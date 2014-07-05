463a9cdc71d5bd7ee355d59db28b9ee2ff9e7b99
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-07-06 02:22:02 +0800

    [Chapter 4] Adding new routes

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,6 +3,14 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+module.exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+module.exports.about = function(req, res){
+  res.render('index', { title: 'About' });
+};
+
+module.exports.signin = function(req, res){
+  res.render('index', { title: 'Sign In' });
 };
