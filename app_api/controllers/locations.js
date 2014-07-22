3afbaf2e4d185878bef21cec7db5a65b6772de8e
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-07-23 01:04:52 +0800

    [Chapter 6] Integrating Mongoose and the model into the controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,3 +1,6 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
 var sendJSONresponse = function(res, status, content) {
     res.status(status);
     res.json(content);  
