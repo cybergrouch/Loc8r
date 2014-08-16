bcd5327c46b48042ad28d425563dd950cff3e04e
cybergrouch <cyber_grouch_lange@yahoo.ie>
2014-08-17 02:53:06 +0800

    Implementing some REST endpoints

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -45,7 +45,42 @@ module.exports.reviewsCreate = function(req, res) {
 };
 
 module.exports.reviewsReadOne = function(req, res) {
-    sendJSONresponse(res, 200, {"status" : "success"});
+    if (req.params && req.params.locationid && req.params.reviewid) {
+        Loc.findById(req.params.locationid)
+           .select("name reviews")
+           .exec(function(err, location) {
+               var response, review;
+               
+               if (!location) {
+                   sendJSONresponse(res, 404, { "message" : "locationid not found" });   
+                   return;
+               } else if (err) {
+                   sendJSONresponse(res, 404, err);   
+                   return;                   
+               }
+               
+               if (location.reviews && location.reviews.length > 0) {
+                   review = location.reviews.id(req.params.reviewid);
+                   if (!review) {
+                       sendJSONresponse(res, 404, { "message" : "reviewid not found" });
+                       return
+                   } else {
+                       response = {
+                           location: {
+                               name: location.name,
+                               id : req.params.locationid
+                           },
+                           review: review
+                       };
+                   }
+               } else {
+                   sendJSONresponse(res, 404, { "message" : "No reviews found" });
+               }
+               sendJSONresponse(res, 200, location);
+           });
+   } else {
+       sendJSONresponse(res, 404, { "message" : "No locationid in request" });       
+   }
 };
 
 module.exports.reviewsUpdateOne = function(req, res) {
